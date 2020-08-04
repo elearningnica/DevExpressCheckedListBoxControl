b2732d8fb0606c65d0b2cb328347e322025d3275
Milton Navarro <milton.j.navarro.p@gmail.com>
2020-08-03 18:49:53 -0600

    Add project files.

---
+++ DevExpressCheckedListBoxControl/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace DevExpressCheckedListBoxControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBoxControl1 = new DevExpress.XtraEditors.CheckedListBoxControl();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.checkedListBoxControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkedListBoxControl1
+            // 
+            this.checkedListBoxControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBoxControl1.Appearance.Options.UseFont = true;
+            this.checkedListBoxControl1.Location = new System.Drawing.Point(86, 125);
+            this.checkedListBoxControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkedListBoxControl1.Name = "checkedListBoxControl1";
+            this.checkedListBoxControl1.Size = new System.Drawing.Size(1324, 344);
+            this.checkedListBoxControl1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select a promotion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1505, 891);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBoxControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.checkedListBoxControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.CheckedListBoxControl checkedListBoxControl1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DevExpressCheckedListBoxControl/Form1.cs
@@ -0,0 +1,37 @@
+﻿using DevExpressCheckedListBoxControl.modelado;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DevExpressCheckedListBoxControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getData();
+        }
+
+        private async void getData()
+        {
+            using (AdventureWorksDW2017Entities entities = new AdventureWorksDW2017Entities())
+            {
+                checkedListBoxControl1.DataSource = await entities.DimPromotion.ToListAsync();
+                checkedListBoxControl1.DisplayMember = "EnglishPromotionName";
+                checkedListBoxControl1.ValueMember = "PromotionKey";
+            }
+        }
+    }
+}
